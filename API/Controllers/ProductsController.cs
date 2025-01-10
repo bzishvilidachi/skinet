c8127d8df70a40a384a0879ba28178c545a0db60
Dachi Bzishvili <bzishvilidachi1@gmail.com>
2025-01-10 22:24:51 +0400

    Add projection to specification pattern

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -73,17 +73,15 @@ public class ProductsController(IGenericRepository<Product> repo) : ControllerBa
   [HttpGet("brands")]
   public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
   {
-   //Implement method later
-
-    return Ok();
+    var spec = new BrandListSpecification();
+    return Ok(await repo.ListAsync(spec));
   }
 
   [HttpGet("types")]
   public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
   {
-     //Implement method later
-
-    return Ok();
+    var spec = new TypeListSpecification();
+    return Ok(await repo.ListAsync(spec));
   }
 
   private bool ProductExists(int id) 
