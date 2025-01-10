b41d122e9d614a69711a38367fcd324202c29594
Dachi Bzishvili <bzishvilidachi1@gmail.com>
2025-01-10 17:31:59 +0400

    Add sorting to specification pattern

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -11,6 +11,14 @@ public class SpecificationEvaluator<T> where T : BaseEntity
     if (spec.Criteria != null){
         query =query.Where(spec.Criteria);
     }
+    if(spec.OrderBy != null){
+      query = query.OrderBy(spec.OrderBy);
+    }
+
+    if(spec.OrderByDescending != null) 
+    {
+      query = query.OrderByDescending(spec.OrderByDescending);
+    }
     return query;
   }
 }
