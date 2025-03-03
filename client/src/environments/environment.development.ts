170583caa8e658cbddfde711270a082afca94cef
Dachi Bzishvili <bzishvilidachi1@gmail.com>
2025-03-03 13:22:58 +0400

    Implement address elements and delivery components

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:5001/api/'
+    apiUrl: 'https://localhost:5001/api/',
+    stripePublicKey: 'pk_test_51QxPNvGdQvqO4cZwN5zDNdFnge7dxbycsHwzISquFtcq9wSpVV6SFt4n52YGj9MObp2QM2dmoATKZZMaL4Vjl3Ju00zZALqRA9'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'api/'
+    apiUrl: 'api/',
+    stripePublicKey: 'pk_test_51QxPNvGdQvqO4cZwN5zDNdFnge7dxbycsHwzISquFtcq9wSpVV6SFt4n52YGj9MObp2QM2dmoATKZZMaL4Vjl3Ju00zZALqRA9'
 };
 
