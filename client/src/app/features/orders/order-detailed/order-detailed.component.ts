95771a2733e96f4c2666da1a96d185a8de62bf87
Dachi Bzishvili <bzishvilidachi1@gmail.com>
2025-03-06 19:22:52 +0400

    Add Stripe Webhook

--- client/src/app/features/orders/order-detailed/order-detailed.component.ts
+++ client/src/app/features/orders/order-detailed/order-detailed.component.ts
@@ -1,19 +1,26 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { OrderService } from '../../../core/services/order.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Order } from '../../../shared/models/order';
 import { MatCardModule } from '@angular/material/card';
 import { MatButton } from '@angular/material/button';
 import { CurrencyPipe, DatePipe } from '@angular/common';
+import { AddressPipe } from "../../../shared/pipes/address.pipe";
+import { CardPipe } from "../../../shared/pipes/card.pipe";
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-order-detailed',
   imports: [
     MatCardModule,
     MatButton,
-    DatePipe, 
-    CurrencyPipe
-  ],
+    DatePipe,
+    CurrencyPipe,
+    AddressPipe,
+    CardPipe,
+    RouterLink, 
+    MatIcon
+],
   templateUrl: './order-detailed.component.html',
   styleUrl: './order-detailed.component.scss'
 })
--- client/src/app/shared/pipes/card.pipe.ts
+++ client/src/app/shared/pipes/card.pipe.ts
@@ -1,20 +1,25 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { ConfirmationToken } from '@stripe/stripe-js';
+import { PaymentSummary } from '../models/order';
 
 @Pipe({
   name: 'card'
 })
 export class CardPipe implements PipeTransform {
 
-  transform(value?: ConfirmationToken['payment_method_preview'], ...args: unknown[]): unknown {
-    if(value?.card){
-      const {brand, exp_month, exp_year, funding, last4} = value.card;
+  transform(value?: ConfirmationToken['payment_method_preview'] | PaymentSummary, ...args: unknown[]): unknown {
+    if (value && 'card' in value) {
+      const { brand, exp_month, exp_year, funding, last4 } = (value as ConfirmationToken['payment_method_preview']).card!;
       return `${brand.toUpperCase()} **** **** **** ${last4}, ${funding} card, Exp: ${exp_month}/${exp_year}`;
 
-  } else {
-    return 'Unknown payment method'
-  }
+    } else if (value && 'last4' in value) {
+      const { brand, expMonth, expYear, last4 } = value as PaymentSummary;
+      return `${brand.toUpperCase()} **** **** **** ${last4}, Exp: ${expMonth}/${expYear}`;
+    } else {
+      return 'Unknown payment method'
     }
+
   }
+}
 
 
